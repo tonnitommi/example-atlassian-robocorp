a714266bac9c430a8ced5b7fcf5cfb42456f6b9c
Mika Hänninen <mika.hanninen@gmail.com>
2022-06-23 11:29:23 +0300

    Add more keywords

--- AtlassianLibrary.py
+++ AtlassianLibrary.py
@@ -1,32 +1,65 @@
+from dataclasses import dataclass
 from atlassian import Jira
 from robot.api import logger
+from RPA.Robocorp.Vault import Vault
+
+
+@dataclass
+class JiraIssue:
+    id: str
+    key: float
+    url: str = None
+
+    def __init__(self, id: str, key: float, url: str):
+        self.id = id
+        self.key = key
+        self.url = f"{url}browse/{self.key}"
+
 
 class AtlassianLibrary:
-
     def __init__(self):
-        return
+        self._jira_url = None
+        self.project = None
 
     def auth_jira_password(self, url, user, token):
         try:
-            self.jira = Jira(
-                url=url,
-                username=user,
-                password=token,
-                cloud=True)
+            self.jira = Jira(url=url, username=user, password=token, cloud=True)
         except Exception as e:
             logger.console(e)
 
-    def create_ticket(self, summary, issuetype):
+    def auth_jira_with_robocorp_vault(self, vault_name: str):
+        secrets = Vault().get_secret(vault_name)
+        self._jira_url = secrets["jira_url"]
+        self.auth_jira_password(
+            self._jira_url, secrets["jira_user"], secrets["jira_token"]
+        )
+
+    def create_ticket(self, summary: str, issuetype: str, project):
+        project = project or self.project
         fields = {
-            'summary': summary,
-            'project': {
-                'id': '10000'
-            },
-            'issuetype': {
-                'id': str(issuetype)
-            }
+            "summary": summary,
+            "project": {"key": project},
+            "issuetype": {"name": issuetype},
         }
         try:
-            self.jira.issue_create(fields)
+            result = self.jira.issue_create(fields)
+            issue = JiraIssue(result["id"], result["key"], url=self._jira_url)
+            return issue
         except Exception as e:
             logger.console(e)
+
+    def get_issue_by_key(self, issue_key: str):
+        return self.jira.issue(issue_key)
+
+    def get_issue_type_names(self):
+        types = self.jira.get_issue_types()
+        result = [{"name": t["name"], "description": t["description"]} for t in types]
+        return result
+
+    def get_issues(self, query: str, project: str = None):
+        project = project or self.project
+        result = self.jira.jql(f"project = {project} AND {query}")
+        return result["issues"]
+
+    def set_jira_project(self, project: str):
+        self.project = project
--- tasks.robot
+++ tasks.robot
@@ -1,18 +1,39 @@
 *** Settings ***
-Library     RPA.Robocorp.Vault
-Library     AtlassianLibrary
-Task Setup    Authorize Atlassian
+Library         RPA.Robocorp.Vault
+Library         AtlassianLibrary
+
+Task Setup      Authorize Atlassian
 
 
 *** Tasks ***
-Minimal task
+Create Issue
     Log To Console    Create Jira issue
     ${summary}    Set Variable    Main order flow on the website is broken on macOS Chrome
-    Create Ticket    ${summary}    10001
+    ${issue}    Create Ticket    ${summary}    Bug    project=BEIS
+    Log To Console    Issue: ${issue}
+
+Get Issue
+    Log To Console    Get Jira issue
+    ${issue}    Get Issue By Key    BEIS-10
+    Log To Console    Issue: ${issue}\n\n
+    Set Jira Project    BEIS
+    ${issues}    Get Issues    status NOT IN (Closed, Resolved) ORDER BY issuekey
+
+    FOR    ${issue}    IN    @{issues}
+        Log To Console    ${issue}[key]    #${issue}[description]\n\n
+    END
+
+List Issue Types
+    Log To Console    List Issue Types
+    ${issue_types}    Get Issue Type Names
+    FOR    ${type}    IN    @{issue_types}
+        Log To Console    ${type}
+    END
+
 
 *** Keywords ***
 Authorize Atlassian
     Log To Console    Create Jira client
-    ${secrets}=    Get Secret    Atlassian
-    Auth Jira Password    ${secrets}[jira_url]    ${secrets}[jira_user]    ${secrets}[jira_token]
-
+    #${secrets}=    Get Secret    Atlassian
+    #Auth Jira Password    ${secrets}[jira_url]    ${secrets}[jira_user]    ${secrets}[jira_token]
+    Auth Jira With Robocorp Vault    Atlassian
